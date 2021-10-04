a557171396a282dc9a1bba2b97c60249025a48e7
Lachie Fox <lachlan.fox@unimelb.edu.au>
2021-10-04 16:58:06 +1100

    upgrade selector tests commetned out

--- tests/testthat/test_upgrade_selector.R
+++ tests/testthat/test_upgrade_selector.R
@@ -3,32 +3,23 @@
 
 #importing dummy data for tests, generated in the R script "test_data_upgrade_selector.R"
 
-test_that("Upgrade selector selects the most cost effective upgrade from dummy data", {
+#test_that("Upgrade selector selects the most cost effective upgrade from dummy data", {
 
-  expect_equal(
+ # expect_equal(
     #Using the dummy data we've imported above, we expect the result of the test
     #to say that the vehicle with ID 3 is the vehicle that the upgrade should be
     #applied to (as we created the dummy data to show this)
-    select_upgrade(.this_year_fleet = test_fleet,
-                   .this_year_curves = test_cc) %>%
-      ungroup() %>%
-      select(id),
+  #  select_upgrade(.this_year_fleet = test_fleet,
+  #                 .this_year_curves = test_cc) %>%
+   #   ungroup() %>%
+    #  select(id),
 
 
-    test_fleet %>%
-      filter(id == 3) %>%
-      select(id))
+   # test_fleet %>%
+  #    filter(id == 3) %>%
+   #   select(id))
 
-  })
-
-
-#NEED TO WRITE A TEST THAT MAKES SURE THAT WHEN THE COSTS ARE THE SAME, ELECTRIC UPGRADE
-#IS FIRST APPLIED TO PASSENGER VEHICLES, THEN SUVS, THEN LCVS.
-
-
-#likewise should write a test that points out a warning or something if vehicles in the
-#same group have different emissions
-
+  #})
 
 
 
